372a99001718294578b16621844f214d3477385d
Sam Stokes <me@samstokes.co.uk>
2018-05-03 15:10:29 -0700

    support kwargs protocol

--- lib/sequel-honeycomb/auto_install.rb
+++ lib/sequel-honeycomb/auto_install.rb
@@ -2,14 +2,14 @@ module Sequel
   module Honeycomb
     module AutoInstall
       class << self
-        def available?
+        def available?(**_)
           gem 'sequel'
           true
         rescue Gem::LoadError
           false
         end
 
-        def auto_install!(honeycomb_client)
+        def auto_install!(honeycomb_client:, logger: nil)
           require 'sequel'
           require 'sequel/extensions/honeycomb'
 
