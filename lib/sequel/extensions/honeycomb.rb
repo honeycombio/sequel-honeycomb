4ff756d73a669dbc3a7b2bbfa6c5a2fb3dc9c0a7
Sam Stokes <sam@honeycomb.io>
2018-05-22 13:11:08 -0500

    Improve field names and test coverage, including non-working cases

--- lib/sequel/extensions/honeycomb.rb
+++ lib/sequel/extensions/honeycomb.rb
@@ -14,49 +14,58 @@ module Sequel
             end
           end
           mod.class_exec(@client) do |honeycomb_|
-            define_method(:honeycomb) { honeycomb_ }
+            define_method(:builder) do
+              honeycomb_.builder.add(
+                'meta.package' => 'sequel',
+                'meta.package_version' => Sequel::VERSION,
+                'type' => 'db',
+              )
+            end
           end
         end
       end
 
       def execute(sql, opts=OPTS, &block)
-        raise 'something went horribly wrong' unless honeycomb # TODO
+        raise 'something went horribly wrong' unless builder # TODO
 
-        event = honeycomb.event
+        event = builder.event
 
-        event.add_field :table, first_source_table rescue nil
-        event.add_field :sql, sql
+        event.add_field 'db.table', first_source_table.to_s rescue nil
+        event.add_field 'db.sql', sql
+        event.add_field 'name', query_name(sql)
         start = Time.now
         adding_span_metadata_if_available(event) do
           super
         end
       rescue Exception => e
         if event
-          event.add_field :exception_class, e.class
-          event.add_field :exception_message, e.message
+          event.add_field 'db.error', e.class.name
+          event.add_field 'db.error_detail', e.message
         end
         raise
       ensure
         if start && event
           finish = Time.now
           duration = finish - start
-          event.add_field :durationMs, duration * 1000
+          event.add_field 'duration_ms', duration * 1000
           event.send
         end
       end
 
       private
+      def query_name(sql)
+        sql.sub(/\s+.*/, '').upcase
+      end
+
       def adding_span_metadata_if_available(event)
         return yield unless defined?(::Honeycomb.trace_id)
 
         trace_id = ::Honeycomb.trace_id
-        name = first_source_table rescue 'sql'
 
         event.add_field :traceId, trace_id if trace_id
         span_id = SecureRandom.uuid
         event.add_field :id, span_id
         event.add_field :serviceName, 'sequel'
-        event.add_field :name, name if name
 
         ::Honeycomb.with_span_id(span_id) do |parent_span_id|
           event.add_field :parentId, parent_span_id
--- spec/extensions/honeycomb_extension_spec.rb
+++ spec/extensions/honeycomb_extension_spec.rb
@@ -2,13 +2,38 @@ require 'support/fakehoney'
 
 require 'sequel/extensions/honeycomb'
 
-RSpec.shared_examples_for 'records a database query' do |name:, sql_match:|
-  it 'records the SQL query' do
-    expect(last_event.data).to include(sql: sql_match)
+RSpec.shared_examples_for 'records a database query' do |name:, sql_match:, sql_not_match: nil|
+  it 'sends a db event' do
+    expect(last_event.data['type']).to eq('db')
   end
 
+  it "sets 'name' to #{name.inspect} (although something more informative would be nicer!)" do
+    expect(last_event.data['name']).to eq(name)
+  end
+
+  it 'records the SQL query' do
+    expect(last_event.data).to include('db.sql' => sql_match)
+  end
+
+  it 'records the table being queried' do
+    expect(last_event.data).to include('db.table' => 'animals')
+  end
+
+  it 'records the parameterised SQL query rather than the literal parameter values' do
+    pending 'find out if this is feasible for Sequel'
+
+    expect(last_event.data['db.sql']).to_not match(sql_not_match)
+  end if sql_not_match
+
   it 'records how long the statement took' do
-    expect(last_event.data[:durationMs]).to be_a Numeric
+    expect(last_event.data['duration_ms']).to be_a Numeric
+  end
+
+  it 'includes meta fields in the event' do
+    expect(last_event.data).to include(
+      'meta.package' => 'sequel',
+      'meta.package_version' => Sequel::VERSION,
+    )
   end
 end
 
@@ -22,6 +47,73 @@ RSpec.describe Sequel::Extensions::Honeycomb do
 
     include_examples 'records a database query',
       name: 'INSERT',
-      sql_match: /^INSERT INTO "animals"/
+      sql_match: /^INSERT INTO "animals"/,
+      sql_not_match: /Lion/
+  end
+
+  context 'after a select' do
+    before do
+      TestDB.Animals.insert name: 'Pooh', species: 'Bear'
+      @sanders = TestDB.Animals.where(species: 'Bear').first
+    end
+
+    include_examples 'records a database query',
+      name: 'SELECT',
+      sql_match: /^SELECT .* FROM "animals"/,
+      sql_not_match: /Bear/
+
+    it 'records how many records were returned' do
+      pending 'depends on underlying adapter API?'
+
+      expect(last_event.data['db.num_rows_returned']).to eq(1)
+    end
+  end
+
+  context 'after an update' do
+    before do
+      @robin_id = TestDB.Animals.insert name: 'Robin Hood', species: 'Fox'
+      TestDB.Animals.where(id: @robin_id).update(name: 'Robert of Loxley')
+    end
+
+    it 'records a database query' do
+      pending 'need to hook in at a different level (Sequel::Database#execute or Sequel::Dataset#execute_dui)'
+      fail "doesn't send events for UPDATE"
+
+      #name: 'UPDATE',
+      #sql_match: /^UPDATE "animals"/,
+      #sql_not_match: /Robert/
+    end
+  end
+
+  context 'after a delete' do
+    before do
+      @robin_id = TestDB.Animals.insert name: 'Robin Hood', species: 'Fox'
+      TestDB.Animals.where(id: @robin_id).delete
+    end
+
+    it 'records a database query' do
+      pending 'need to hook in at a different level (Sequel::Database#execute or Sequel::Dataset#execute_dui)'
+      fail "doesn't send events for DELETE"
+
+      #name: 'DELETE',
+      #sql_match: /^DELETE FROM "animals"/
+    end
+  end
+
+  context 'if the database raises an error' do
+    before do
+      expect { TestDB.Animals.where(habitat: 'jungle').count }.to raise_error(Sequel::DatabaseError, /habitat/)
+    end
+
+    it 'records the exception' do
+      expect(last_event.data).to include(
+        'db.error' => 'Sequel::DatabaseError',
+        'db.error_detail' => /habitat/,
+      )
+    end
+
+    it 'still records how long the statement took' do
+      expect(last_event.data['duration_ms']).to be_a Numeric
+    end
   end
 end
