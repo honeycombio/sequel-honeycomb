fd171accfc9c8852c51aae3c10605b6766885c8c
Martin Holman <me@martinholman.co.nz>
2018-11-27 11:50:32 -0800

    Support more scenarios
    Populate name earlier so that the gem can be used without the beeline.
    Support multiple beeline versions

--- lib/sequel/extensions/honeycomb.rb
+++ lib/sequel/extensions/honeycomb.rb
@@ -21,6 +21,7 @@ module Sequel
           @builder ||= @client.builder.add(
             'meta.package' => 'sequel',
             'meta.package_version' => Sequel::VERSION,
+            'type' => 'db',
           )
         end
 
@@ -39,8 +40,9 @@ module Sequel
 
         event.add_field 'db.table', first_source_table.to_s rescue nil
         event.add_field 'db.sql', sql
+        event.add_field 'name', query_name(sql)
         start = Time.now
-        with_tracing_if_available(event, query_name(sql)) do
+        with_tracing_if_available(event) do
           super
         end
       rescue Exception => e
@@ -63,10 +65,27 @@ module Sequel
         sql.sub(/\s+.*/, '').upcase
       end
 
-      def with_tracing_if_available(event, name)
-        return yield unless defined?(::Honeycomb::Beeline::VERSION)
+      def with_tracing_if_available(event)
+        # return if we are not using the ruby beeline
+        return yield unless defined?(::Honeycomb)
 
-        ::Honeycomb.span_for_existing_event(event, name: name, type: 'db') do
+        # beeline version <= 0.5.0
+        if ::Honeycomb.respond_to? :trace_id
+          trace_id = ::Honeycomb.trace_id
+          event.add_field 'trace.trace_id', trace_id if trace_id
+          span_id = SecureRandom.uuid
+          event.add_field 'trace.span_id', span_id
+          ::Honeycomb.with_span_id(span_id) do |parent_span_id|
+            event.add_field 'trace.parent_id', parent_span_id
+            yield
+          end
+        # beeline version > 0.5.0
+        elsif ::Honeycomb.respond_to? :span_for_existing_event
+          ::Honeycomb.span_for_existing_event(event, name: nil, type: 'db') do
+            yield
+          end
+        # fallback if we don't detect any known beeline tracing methods
+        else
           yield
         end
       end
