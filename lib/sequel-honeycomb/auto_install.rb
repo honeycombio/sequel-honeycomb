b00188b9d1c603657f48acf12b1428264003e0e4
Martin Holman <me@martinholman.co.nz>
2018-12-12 12:56:48 -0800

    Also remove Sequel constant here
    Although we remove the Sequel constant when the gem is required, the
    beeline still calls auto_install which recreates the Sequel constant at
    a later time. Depending on initialization timing of other gems that look
    for the Sequel constant still don't work

--- lib/sequel-honeycomb/auto_install.rb
+++ lib/sequel-honeycomb/auto_install.rb
@@ -8,6 +8,10 @@ module Sequel
           true
         rescue Gem::LoadError => e
           logger.debug "Didn't detect Sequel (#{e.class}: #{e.message}), not autoinitialising sequel-honeycomb" if logger
+          # some gems use the presence of the Sequel module to determine if Sequel is in
+          # use by the application. If we can't require the gem then we need to remove
+          # the constant that we define so that other gems don't make bad assumptions
+          Object.send(:remove_const, "Sequel")
           false
         end
 
